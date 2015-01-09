1101b4f99bd373f784a5a79598133e6163737992
LuoYaoSheng <lys1988_cool@me.com>
2015-01-10 00:32:21 +0800

    添加手势层操作。

---
+++ PopStart/Classes/TouchLayer.cpp
@@ -0,0 +1,9 @@
+//
+//  TouchLayer.cpp
+//  lys
+//
+//  Created by 罗耀生 on 15/1/9.
+//
+//
+
+#include "TouchLayer.h"
---
+++ PopStart/Classes/TouchLayer.h
@@ -0,0 +1,14 @@
+//
+//  TouchLayer.h
+//  lys
+//
+//  Created by 罗耀生 on 15/1/9.
+//
+//
+
+#ifndef __lys__TouchLayer__
+#define __lys__TouchLayer__
+
+#include <stdio.h>
+
+#endif /* defined(__lys__TouchLayer__) */
