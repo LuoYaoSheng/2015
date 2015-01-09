378bc14502a204c96f5f0883d40b75d43d23fdce
LuoYaoSheng <lys1988_cool@me.com>
2015-01-10 01:24:01 +0800

    考虑添加一个专门针对单个控件的操作类。

---
+++ PopStart/Classes/TestSprite.cpp
@@ -0,0 +1,9 @@
+//
+//  TestSprite.cpp
+//  lys
+//
+//  Created by 罗耀生 on 15/1/10.
+//
+//
+
+#include "TestSprite.h"
---
+++ PopStart/Classes/TestSprite.h
@@ -0,0 +1,14 @@
+//
+//  TestSprite.h
+//  lys
+//
+//  Created by 罗耀生 on 15/1/10.
+//
+//
+
+#ifndef __lys__TestSprite__
+#define __lys__TestSprite__
+
+#include <stdio.h>
+
+#endif /* defined(__lys__TestSprite__) */
