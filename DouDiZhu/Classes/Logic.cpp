dca89a15fd355f1af821642f02108473e5f30d23
LuoYaoSheng <lys1988_cool@me.com>
2015-01-26 01:05:25 +0800

    1. 算法修正到一半。

--- DouDiZhu/Classes/Logic.cpp
+++ DouDiZhu/Classes/Logic.cpp
@@ -35,61 +35,62 @@ void Logic::initCardList(unsigned char cbCardData[], unsigned char cbCardCount)
 //控制函数
 //排列扑克
 void Logic::SortCardList(unsigned char cbCardData[], unsigned char cbCardCount){
+    
    	//转换数值
-	unsigned char cbLogicValue[FULL_COUNT];
-	for (unsigned char i=0;i<cbCardCount;i++)
-		cbLogicValue[i]=GetCardLogicValue(cbCardData[i]);
+    unsigned char cbLogicValue[FULL_COUNT];
+    for (unsigned char i=0;i<cbCardCount;i++)
+        cbLogicValue[i]=GetCardLogicValue(cbCardData[i]);
     
-	//排序操作
-	unsigned char bSorted=1;
-	unsigned char cbTempData,bLast=cbCardCount-1;
-	do
-	{
-		bSorted=1;
-		for (unsigned char i=0;i<bLast;i++)
-		{
-			if ((cbLogicValue[i]<cbLogicValue[i+1])||
-				((cbLogicValue[i]==cbLogicValue[i+1])&&(cbCardData[i]<cbCardData[i+1])))
-			{
-				//交换位置
-				cbTempData=cbCardData[i];
-				cbCardData[i]=cbCardData[i+1];
-				cbCardData[i+1]=cbTempData;
-				cbTempData=cbLogicValue[i];
-				cbLogicValue[i]=cbLogicValue[i+1];
-				cbLogicValue[i+1]=cbTempData;
-				bSorted=0;
-			}
-		}
-		bLast--;
-	} while(bSorted==0);
+    //排序操作
+    unsigned char bSorted=1;
+    unsigned char cbTempData,bLast=cbCardCount-1;
+    do
+    {
+        bSorted=1;
+        for (unsigned char i=0;i<bLast;i++)
+        {
+            if ((cbLogicValue[i]<cbLogicValue[i+1])||
+                ((cbLogicValue[i]==cbLogicValue[i+1])&&(cbCardData[i]<cbCardData[i+1])))
+            {
+                //交换位置
+                cbTempData=cbCardData[i];
+                cbCardData[i]=cbCardData[i+1];
+                cbCardData[i+1]=cbTempData;
+                cbTempData=cbLogicValue[i];
+                cbLogicValue[i]=cbLogicValue[i+1];
+                cbLogicValue[i+1]=cbTempData;
+                bSorted=0;
+            }
+        }
+        bLast--;
+    } while(bSorted==0);
     
-	return;
+    return;
 }
 //混乱扑克
 void Logic::RandCardList(unsigned char cbCardBuffer[], unsigned char cbBufferCount){
-	unsigned char cbCardData[CountArray(m_cbCardData)];
-	memcpy(cbCardData,m_cbCardData,sizeof(m_cbCardData));
+    unsigned char cbCardData[CountArray(m_cbCardData)];
+    memcpy(cbCardData,m_cbCardData,sizeof(m_cbCardData));
     
-	//混乱扑克
-	unsigned char cbRandCount=0,cbPosition=0;
-	do
-	{
-		cbPosition = arc4random()%(cbBufferCount-cbRandCount);
-		cbCardBuffer[cbRandCount++]=cbCardData[cbPosition];
-		cbCardData[cbPosition]=cbCardData[cbBufferCount-cbRandCount];
-	} while (cbRandCount < cbBufferCount);
+    //混乱扑克
+    unsigned char cbRandCount=0,cbPosition=0;
+    do
+    {
+        cbPosition = arc4random()%(cbBufferCount-cbRandCount);
+        cbCardBuffer[cbRandCount++]=cbCardData[cbPosition];
+        cbCardData[cbPosition]=cbCardData[cbBufferCount-cbRandCount];
+    } while (cbRandCount < cbBufferCount);
     
-	return;
+    return;
 }
 
 //功能函数
 //逻辑数值
 unsigned char Logic::GetCardLogicValue(unsigned char cbCardData){
     //扑克属性
-	unsigned char bCardValue=GetCardValue(cbCardData);
+    unsigned char bCardValue=GetCardValue(cbCardData);
     
-	//转换数值
+    //转换数值
     switch ( bCardValue) {
         case 1:
             bCardValue += 13;
@@ -100,7 +101,7 @@ unsigned char Logic::GetCardLogicValue(unsigned char cbCardData){
         default:
             break;
     }
-	return bCardValue;
+    return bCardValue;
 }
 
 unsigned char Logic::GetCardValue(unsigned char cbCardData){
@@ -109,5 +110,174 @@ unsigned char Logic::GetCardValue(unsigned char cbCardData){
 
 unsigned char Logic::GetCardColor(unsigned char cbCardData){
     return cbCardData&LOGIC_MASK_COLOR;
+}
+
+
+unsigned char Logic::GetCardType(unsigned char cbCardData[], unsigned char cbCardCount)
+{
+    //避免影响参数
+    unsigned char temp[ cbCardCount ];
+    memcpy(temp, cbCardData, cbCardCount);
+    SortCardList(temp, cbCardCount);
+    
+    
+    //快速写法，完成后再考虑优化问题
+    
+    switch ( cbCardCount ) {
+        case 1:
+        {
+            return eA;
+        }
+            break;
+        case 2:
+        {
+            if ( GetCardValue( temp[0] ) == GetCardValue( temp[1] ) ) {
+                if ( temp[0] == 0x41 || temp[0] == 0x42 ) {
+                    return eKK;
+                }
+                return eAA;
+            }
+        }
+            break;
+        case 3:
+        {
+            if ( GetCardValue( temp[0]) == GetCardValue( temp[1]) && GetCardValue( temp[0]) == GetCardValue( temp[2]) ) {
+                return eAAA;
+            }
+        }
+            break;
+        case 4:
+        {
+            int icount = -1;//扣除自己
+            for (int idx = 0; idx < cbCardCount; idx++) {
+                if ( GetCardValue( temp[idx]) == GetCardValue( temp[1]) ) {
+                    icount++;
+                }
+            }
+            if ( icount == 3) return eAAAB;
+            if ( icount == 4) return eAAAA;
+        }
+            break;
+            
+        case 5:
+        {
+            //是不是顺子 --- A、2、大小王 不能组成顺子
+            //三带二 -- 两个不能是大小王
+            //优先检测是否包含 三张 ，大小王
+            
+            int icount = -1;//扣除自己
+            for (int idx = 0; idx < cbCardCount; idx++) {
+                if ( GetCardValue( temp[idx]) == GetCardValue( temp[2]) ) {
+                    icount++;
+                }
+            }
+            if ( icount == 3){
+                if (  (GetCardValue( temp[0]) == GetCardValue( temp[2]) &&  GetCardValue( temp[3]) == GetCardValue( temp[4])  )
+                    || (GetCardValue( temp[3]) == GetCardValue( temp[2]) &&  GetCardValue( temp[0]) == GetCardValue( temp[1])) ) {
+                    return eAAABB;
+                }
+            }else if( icount == 1){
+                //从大到小
+                if (!( temp[0] == 0x41 || temp[0] == 0x42 || GetCardValue( temp[0] ) == 1|| GetCardValue( temp[0] ) == 2) ) {
+                    if ( 1 == temp[0]-temp[1] && 1 == temp[1]-temp[2] && 1 == temp[2]-temp[3] && 1 == temp[3]-temp[4] ) {
+                        return eABCDEF;
+                    }
+                }
+            }
+        }
+            break;
+        case 6:
+        {
+            int icount = -1;//扣除自己
+            int iOther = -1;
+            for (int idx = 0; idx < cbCardCount; idx++) {
+                if ( GetCardValue( temp[idx]) == GetCardValue( temp[3]) ) {
+                    icount++;
+                    iOther = idx;
+                }
+            }
+            if ( icount == 4 ) {
+                if ( (iOther == 5 && (temp[0] != 0x41 || temp[0] != 0x42) && GetCardValue(temp[0]) != GetCardValue(temp[1]) )
+                    || ( iOther == 4 && (temp[0] != 0x41 || temp[0] != 0x42) && GetCardValue(temp[0]) != GetCardValue(temp[5]) )
+                    || ( iOther == 3  && GetCardValue(temp[4]) != GetCardValue(temp[5]) ) ){
+                    return eAAAABC;
+                }
+            }else if ( icount == 3){
+                if ( (GetCardValue(temp[0]) == GetCardValue(temp[1]) && GetCardValue(temp[1]) == GetCardValue(temp[2]))
+                    && (GetCardValue(temp[3]) == GetCardValue(temp[4]) && GetCardValue(temp[4]) == GetCardValue(temp[5])) ) {
+                    return eAAABBB;
+                }
+            }else if ( icount == 2){
+                
+                if ( GetCardValue(temp[0]) == GetCardValue(temp[1]) && GetCardValue(temp[2]) == GetCardValue(temp[3]) && GetCardValue(temp[4]) == GetCardValue(temp[5]) &&  GetCardValue(temp[0]) - GetCardValue(temp[2]) == 1 &&  GetCardValue(temp[2]) - GetCardValue(temp[4]) == 1)  {
+                    return eAABBCC;
+                }
+                
+            }else if ( icount == 1){
+                
+                if (!( temp[0] == 0x41 || temp[0] == 0x42 || GetCardValue( temp[0] ) == 1|| GetCardValue( temp[0] ) == 2) ) {
+                    if ( 1 == temp[0]-temp[1] && 1 == temp[1]-temp[2] && 1 == temp[2]-temp[3] && 1 == temp[3]-temp[4] && 1 == temp[4]-temp[5] ) {
+                        return eABCDEF;
+                    }
+                }
+                
+            }
+        }
+            break;
+            case 7:
+        {
+            if (!( temp[0] == 0x41 || temp[0] == 0x42 || GetCardValue( temp[0] ) == 1|| GetCardValue( temp[0] ) == 2) ) {
+                if ( 1 == temp[0]-temp[1] && 1 == temp[1]-temp[2] && 1 == temp[2]-temp[3] && 1 == temp[3]-temp[4] && 1 == temp[4]-temp[5] && 1 == temp[5]-temp[6] ) {
+                    return eABCDEF;
+                }
+            }
+        }
+            break;
+            
+            case 8:
+        {
+            int icount = -1;//扣除自己
+            int iOther = -1;
+            for (int idx = 0; idx < cbCardCount; idx++) {
+                if ( GetCardValue( temp[idx]) == GetCardValue( temp[3]) ) {
+                    icount++;
+                    iOther = idx;
+                }
+            }
+            
+            if ( icount == 4) {
+                
+            }
+            
+        }
+            break;
+        default:
+        {
+//            eAAAABBCC,                                                         8  //四带二对：必须带两个对牌，对子可以不用相连。如 8888+7799)
+//            eAABBCC,                                                         >=6 -- 2*n && n > 2  //对顺 ( 连对 ) ：必须 3 对或 3 对以上连续的对牌，且其中不能有 2 或大小王 ( 如 667788 或 3344556677)
+//            eAAABBB,                                                         >=6  //三顺（连三条）：必须 2 个或 2 个以上连续的三张牌，且其中不能有 2 或大小王：如： 333444 、 555666777888 ）
+//            eAAABBBCD,                                                       >=8  //三顺带一：必须 2 个或 2 个以上连续的三张牌，加上相同牌数的任意单牌，且任意单牌中不能含有大小王（如： 444555+79 ）
+//            eAAABBBCCDD,                                                     >=10 //三顺带对：类似三顺带一，区别是带牌必须为对牌 （如： 444555+77 或 333444555+7799JJ ）
+            
+            
+            
+        }
+            
+            break;
+    }
+    return eErr;
+}
+bool Logic::GetCardMini(unsigned char dest[],unsigned char src[], unsigned char output[], int count)
+{
+    //获取 源 牌型 以及最大牌
+    int srcType = Logic::GetCardType( src, count);
+    
+    
+    
+    return false;
+}
+bool Logic::GetValid(unsigned char cbCardData[], int count)
+{
+    return Logic::GetCardType( cbCardData, count);
 }
 
--- DouDiZhu/Classes/Logic.h
+++ DouDiZhu/Classes/Logic.h
@@ -24,58 +24,41 @@
 #define	LOGIC_MASK_VALUE			0x0F								//数值掩码
 
 //扑克类型
-#define CT_SINGLE				1									//单牌类型
-#define CT_ONE_LONG				2									//对子类型
-#define CT_TWO_LONG				3									//两对类型
-#define CT_THREE_TIAO			4									//三条类型
-#define	CT_SHUN_ZI				5									//顺子类型
-#define CT_TONG_HUA				6									//同花类型
-#define CT_HU_LU				7									//葫芦类型
-#define CT_TIE_ZHI				8									//铁支类型
-#define CT_TONG_HUA_SHUN		9									//同花顺型
-#define CT_KING_TONG_HUA_SHUN	10									//皇家同花顺
-
-//胜利信息结构
-typedef struct
-{
-	unsigned char bSameCount;
-	unsigned char wWinerList[GAME_PLAYER];
-}UserWinList;
-
-//分析结构
-typedef struct
-{
-	unsigned char 							cbFourCount;					//四张数目
-	unsigned char 							cbThreeCount;					//三张数目
-	unsigned char 							cbLONGCount;					//两张数目
-	unsigned char							cbSignedCount;					//单张数目
-	unsigned char 							cbFourLogicVolue[1];				//四张列表
-	unsigned char 							cbThreeLogicVolue[1];				//三张列表
-	unsigned char 							cbLONGLogicVolue[2];				//两张列表
-	unsigned char 							cbSignedLogicVolue[5];				//单张列表
-	unsigned char							cbFourCardData[MAX_CENTERCOUNT];		//四张列表
-	unsigned char							cbThreeCardData[MAX_CENTERCOUNT];		//三张列表
-	unsigned char							cbLONGCardData[MAX_CENTERCOUNT];		//两张列表
-	unsigned char							cbSignedCardData[MAX_CENTERCOUNT];		//单张数目
-}tagAnalyseResult;
-
+enum CT_Type{
+    eErr  = 0,
+    eA = 1,                                                             //单张：可以是手中的任意一张牌 （如 ◆ 8 ）
+    eAA,                                                                //一对：两张牌点相同的牌，两张牌的花色可以不同（如 88 ）
+    eAAA,                                                               //三同张：三张牌点相同的牌，三张牌的花色可以不同（如 888 ）
+    eAAAB,                                                              //三带一：三张牌点相同的牌和另外一张牌一起出。 ( 如 888+9)
+    eAAABB,                                                             //三带二：三张牌点相同的牌和另外两张牌点相同的牌一起出。 ( 如 888+99)
+    eAAAABC,                                                            //四带二单：必须带两张单牌，不能带大王，小王 ( 如 8888+79)
+    eAAAABBCC,                                                          //四带二对：必须带两个对牌，对子可以不用相连。如 8888+7799)
+    eABCDEF,                                                            //单顺：必须 5 张或 5 张以上连续的单牌，且其中不能有 2 或大小王 ( 如 45678 或 10JQKA)
+    eAABBCC,                                                            //对顺 ( 连对 ) ：必须 3 对或 3 对以上连续的对牌，且其中不能有 2 或大小王 ( 如 667788 或 3344556677)
+    eAAABBB,                                                            //三顺（连三条）：必须 2 个或 2 个以上连续的三张牌，且其中不能有 2 或大小王：如： 333444 、 555666777888 ）
+    eAAABBBCD,                                                          //三顺带一：必须 2 个或 2 个以上连续的三张牌，加上相同牌数的任意单牌，且任意单牌中不能含有大小王（如： 444555+79 ）
+    eAAABBBCCDD,                                                        //三顺带对：类似三顺带一，区别是带牌必须为对牌 （如： 444555+77 或 333444555+7799JJ ）
+    eAAAA,                                                              //炸弹：四张一样牌点的牌（如 8888 ）
+    eKK,                                                                //火箭：大王跟小王一起为火箭
+};
 
 class Logic {
-public:    
+public:
     //初始化扑克数据
     static void initCardList(unsigned char cbCardData[], unsigned char cbCardCount);
     static void SortCardList(unsigned char cbCardData[], unsigned char cbCardCount);
     static void RandCardList(unsigned char cbCardBuffer[], unsigned char cbBufferCount);
     
-    //功能函数 --
+    //功能函数 -- 基本
     static unsigned char GetCardLogicValue(unsigned char cbCardData);
     static unsigned char GetCardValue(unsigned char cbCardData);
     static unsigned char GetCardColor(unsigned char cbCardData);
     
-    
-    
-    
-    
+    //功能函数
+    static unsigned char GetCardType(unsigned char cbCardData[], unsigned char cbCardCount);                    //获得牌型
+    static bool GetCardMini(unsigned char dest[],unsigned char src[], unsigned char output[], int count);        //获得压过对方最小牌型 -- 没有则返回空数组
+    static bool GetValid(unsigned char cbCardData[], int count);                                                //选中牌是否有效
+
 };
 
 #endif /* defined(__texasodds__Logic__) */
